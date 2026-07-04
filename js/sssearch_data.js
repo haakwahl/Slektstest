dfc01bd6987733864a9018af526a1ae2d6a4a9af
haakwahl <haakwahl@hotmail.com>
2026-07-04 22:10:33 +0200

    Test

---
+++ js/sssearch_data.js
@@ -0,0 +1,3 @@
+ss_search_data.people={"1":{u:[1,1,1,154],g:"m",n:[[1,2]],f:2,m:3,s:[4]},"2":{u:[1,1,2],g:"m",n:[[1,3]],s:[3]},"3":{u:[1,1,3],g:"f",n:[[4,5]],s:[2]},"4":{u:[1,1,4,158],g:"f",n:[[6,7]],s:[1]}};
+
+ss_search_data.strings=["","Wahl","Håkon","Steinar","Hansen","Jenny","Eggan","Lovise Julie"]
